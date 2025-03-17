cbb6eb37dd88e94ee395088d1cfb88357f568833
rohith <reddyrohith20061902@gmail.com>
2025-03-17 15:40:36 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello")
+import cv2
+import time
+import os
+from eye_movement import process_eye_movement
+from head_pose import process_head_pose
+from mobile_detection import process_mobile_detection
+
+cap = cv2.VideoCapture(0)
