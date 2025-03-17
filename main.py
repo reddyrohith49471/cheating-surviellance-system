c1166ebc28f2add2a181c2c0d999ff9536b3905a
rohith <reddyrohith20061902@gmail.com>
2025-03-17 15:46:08 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import cv2
-import time
-import os
-from eye_movement import process_eye_movement
-from head_pose import process_head_pose
-from mobile_detection import process_mobile_detection
+# import time
+# import os
+# from eye_movement import process_eye_movement
+# from head_pose import process_head_pose
+# from mobile_detection import process_mobile_detection
 
+
+
+# Open the webcam
 cap = cv2.VideoCapture(0)
+
+
